3a36a9e701f1a7ccfe1b86f251161e3f758d30cc
Daniel <Daniel@DanielP>
2017-12-23 17:59:55 -0500

    Practica004 para el 02/01/2018 Base de Datos File Object

--- src/Practica004/vista/VentanaPrincipal.java
+++ src/Practica004/vista/VentanaPrincipal.java
@@ -5,10 +5,39 @@
  */
 package Practica004.vista;
 
+import Practica004.controlador.GestionDato;
+import java.util.ArrayList;
+import javax.swing.JDesktopPane;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
 /**
  *
  * @author Daniel
  */
-public class VentanaPrincipal {
+public class VentanaPrincipal extends JFrame
+{
+    private GestionDato gestionDato;
+    private JDesktopPane escritorio;
+    private JMenuBar barraMenu;
+    private java.util.List<JMenu> menuList;
+    private ArrayList<JMenuItem> menuItemList; 
     
+    public VentanaPrincipal(GestionDato gD) 
+    {
+        super("Archivos");
+        this.gestionDato = gD;
+        this.setSize(800, 600);
+        this.setLocation(500,250);
+        this.iniciaComponente();
+        this.setDefaultCloseOperation(3);
+        this.setVisible(true);      
+    }
+    
+    public void iniciaComponente()
+    {
+        
+    }
 }
